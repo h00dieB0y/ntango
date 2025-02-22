c1cdbf856d184b16f30f036c84d999d498c1f59c
hoddieBoy <emilemannekitsoukou@gmail.com>
2025-02-22 08:16:12 +0100

    init: Initialize Spring Boot project with Maven setup and basic application structure

---
+++ src/main/java/engineer/mkitsoukou/ntango/NtangoApplication.java
@@ -0,0 +1,13 @@
+package engineer.mkitsoukou.ntango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtangoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NtangoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/engineer/mkitsoukou/ntango/NtangoApplicationTests.java
@@ -0,0 +1,13 @@
+package engineer.mkitsoukou.ntango;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtangoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
